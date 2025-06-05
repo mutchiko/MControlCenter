00925b95717d33c9692289d74b2c9676911c0f3a
Mutchiko <95985922+mutchiko@users.noreply.github.com>
2025-06-05 12:55:27 +0200

    Instance Detection: Use D-Bus instead of network
    simplifies detection and drops the need for Network entirely

--- src/main.cpp
+++ src/main.cpp
@@ -20,21 +20,23 @@
 
 #include <QApplication>
 #include <QTranslator>
-#include <QLocalSocket>
-#include <QLocalServer>
-
+#include <QDBusConnectionInterface>
 
 int main(int argc, char *argv[]) {
-    const QString serverName = "MControlCenter";
-    auto *socket = new QLocalSocket();
-    socket->connectToServer(serverName);
-    if (socket->isOpen()) {
-        fprintf(stderr, "Another instance of the application is already running\n");
-        socket->close();
-        socket->deleteLater();
+    const QString serviceName = "org.MControl.Center";
+
+    if (QDBusConnection::sessionBus().interface()->isServiceRegistered(serviceName)) {
+                fprintf(stderr, "Another instance of the application is already running\n");
         return 0;
     }
-    socket->deleteLater();
+
+    if (!QDBusConnection::sessionBus().registerService(serviceName)) {
+            if (!QDBusConnection::sessionBus().registerService(serviceName)) {
+                fprintf(stderr, "Faild to register D-Bus service\n");
+        return 1;
+    }
+        return 1;
+    }
 
     QApplication a(argc, argv);
 
@@ -49,16 +51,6 @@ int main(int argc, char *argv[]) {
     }
 
     MainWindow w;
-
-    QLocalServer server;
-    QObject::connect(&server, &QLocalServer::newConnection, [&w]() {
-        w.show();
-    });
-    bool serverListening = server.listen(serverName);
-    if (!serverListening && (server.serverError() == QAbstractSocket::AddressInUseError)) {
-        QLocalServer::removeServer(serverName);
-        server.listen(serverName);
-    }
 
     return QApplication::exec();
 }
